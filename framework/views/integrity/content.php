310728f0225ffea91e051d26735debdc063d45f3
Bobby Bosler <bobbybosler@gmail.com>
2019-07-29 16:28:15 -0400

    Changed structure of single post content. We'll see if this works!

--- framework/views/integrity/content.php
+++ framework/views/integrity/content.php
@@ -10,29 +10,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <?php if ( is_single() ) : ?>
-  <?php /* bcm_category_header(); */ ?>
   <div class="article-wrap">
-<?php /*
-    <div class="entry-featured">
-      <?php x_featured_image(); ?>
+    <div class="entry-wrap">
+      <?php x_get_view( 'global', '_content' ); ?>
     </div>
-*/ ?>
+  </div>
+  <?php x_get_view( 'integrity', '_content', 'post-footer' );?>
   <?php else : ?>
   <div class="entry-featured" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_ID(), 'entry', NULL ); ?>')">
     <div class="entry-overlay">
     </div>
   </div>
-  <?php endif; ?>
-    <div class="entry-wrap">
-      <?php if ( is_single() ) : ?>
-          <?php x_get_view( 'global', '_content' ); ?>
-      <?php else : ?>
-          <?php x_get_view( 'integrity', '_content', 'post-header' ); ?>
-          <?php x_get_view( 'global', '_content' ); ?>
-    </div>
-  <?php endif; ?>  
-  <?php if ( is_single() ) : ?>
+  <div class="entry-wrap">
+    <?php x_get_view( 'integrity', '_content', 'post-header' ); ?>
+    <?php x_get_view( 'global', '_content' ); ?>
+    <?php x_get_view( 'integrity', '_content', 'post-footer' ); ?>
   </div>
-  <?php endif; ?>
-  <?php x_get_view( 'integrity', '_content', 'post-footer' ); ?>
+<?php endif; ?>
 </article>
